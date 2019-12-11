593807326897f3dfbbacc4096f13c5919dbc61aa
Trent Novelly <trentnov@gmail.com>
2019-12-10 23:29:21 -0500

    adjusting goal state

--- temp2.m
+++ temp2.m
@@ -7,7 +7,7 @@ time_to_swing = 1;
 dt = time_to_swing/naughtPoints;
 
 init_cond = [-pi/4;0;pi;0;0;0];
-goal_cond = [pi/4;-pi/4;3*pi/4;0;0;0];
+goal_cond = [pi/4;-pi/4;pi/2;0;0;0];
 
 %initial guess is 0
 u0 = zeros(2,naughtPoints);
--- temp3.m
+++ temp3.m
@@ -9,7 +9,7 @@ time_to_swing = 1;
 dt = time_to_swing/naughtPoints;
 
 init_cond = [-pi/4;0;pi;0;0;0];
-goal_cond = [pi/4;-pi/4;3*pi/4;0;0;0];
+goal_cond = [pi/4;-pi/4;pi/2;0;0;0];
 
 %use a better guess
 second_guess = [zeros(2,naughtPoints);short_traj(1:3,:);zeros(3,naughtPoints)];
